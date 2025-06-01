167b5b47b796fe1f402a81c372cbb0300019d114
asseternity <nakupovasset@gmail.com>
2025-06-01 17:01:47 +0400

    feat: update favicon and browser tab name

--- webpack.common.js
+++ webpack.common.js
@@ -1,44 +1,45 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: path.resolve(__dirname, 'src/comp_story_functions.js'),
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-            filename: 'index.html',
-            title: `You Do God's Work - Text RPG`,
-        })
+  entry: {
+    index: path.resolve(__dirname, "src/comp_story_functions.js"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+      title: `What Makes You Live? | RPG`,
+      favicon: path.resolve(__dirname, "src/adventure-map-svgrepo-com.svg"),
+    }),
+  ],
+  watchOptions: {
+    ignored: /node_modules/,
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+  output: {
+    filename: "[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
-    watchOptions: {
-        ignored: /node_modules/,
-        aggregateTimeout: 300,
-        poll: 1000
-    },
-    output: {
-        filename: '[contenthash].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
+  },
 };
